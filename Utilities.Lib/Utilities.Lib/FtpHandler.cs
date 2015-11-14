b3950ad8d1d9f76f25de41641e4e84fb6fd092cb
montyaahad <montyaahad@gmail.com>
2015-11-14 18:52:50 +0600

    initial commit
    initial commit; ftp file upload

---
+++ Utilities.Lib/Utilities.Lib/FtpHandler.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities.Lib
+{
+    public static class FtpHandler
+    {
+        public static void UploadFile(string ftpurl, string ftpusername, string ftppassword, string filename, byte[] buffer)
+        {
+            try
+            {
+                //String ftpurl = "ftp://serverip/foldername/foldername/";
+                //String ftpusername = "username";
+                //String ftppassword = "password";
+                //string filename = "file.txt";
+
+                string ftpfullpath = ftpurl + filename;
+                FtpWebRequest ftp = (FtpWebRequest)FtpWebRequest.Create(ftpfullpath);
+                ftp.Credentials = new NetworkCredential(ftpusername, ftppassword);
+
+                ftp.KeepAlive = true;
+                ftp.UseBinary = true;
+                ftp.Method = WebRequestMethods.Ftp.UploadFile;
+
+                using (Stream ftpstream = ftp.GetRequestStream())
+                {
+                    ftpstream.Write(buffer, 0, buffer.Length);
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
